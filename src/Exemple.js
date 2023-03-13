4aa8d11519d888a899d95a7e61487a54afe4365e
Hajima <vdhajima@gmail.com>
2023-03-13 12:06:38 +0200

    Monstr_rolodex the end

---
+++ src/Exemple.js
@@ -0,0 +1,10 @@
+function petia (a){
+    const b=10+a ,
+   
+    c=b+a, 
+  
+     m=c+b
+ 
+}
+ 
+console.log(b);
---
+++ src/components/card/card.components.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import './card.style.css';
+
+
+class Card extends Component{
+render(){
+    const{id ,name , email}=this.props.monster;
+    return(
+        <div className="card-container" key={id}>
+                    <img alt={`monster ${name}`}
+                        src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+                    />
+                    <h2 className="title">{name}</h2>
+                    <p className="mail">{email}</p>
+                </div>
+    )
+}
+}
+export default Card;
